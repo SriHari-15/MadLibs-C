2b9c744dd508313d3bb435a823f3297309adfac7
SriHari-15 <sriharidev1508@gmail.com>
2025-01-14 15:58:41 +0530

    Push to Public

---
+++ madlibs.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char color[10];
+    char noun[15];
+    char name[10];
+
+    printf("Enter a color: ");
+    fgets(color, sizeof(color), stdin);
+    color[strcspn(color, "\n")] = '\0';
+
+    printf("Enter a noun: ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strcspn(noun, "\n")] = '\0';
+
+    printf("Enter a name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = '\0';
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", noun);
+    printf("I love %s\n", name);
+
+    return 0;
+}
